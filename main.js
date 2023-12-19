0ee50f54960ef89dd6608a311d51ea6c50fddcac
Megan Crotteau <megan.crotteau@gmail.com>
2023-12-18 23:08:33 -0700

    Random recipe generator

--- main.js
+++ main.js
@@ -8,24 +8,67 @@ var mainDishes = ['Chicken Shawarma', 'Lamb Kofta', 'Paella', 'Gyro Platter', 'M
 var desserts = ['Baklava', 'Tiramisu', 'Creme brulee', 'Cheesecake', 'Chocolate Cake']
 var entireMeal = []
 var currentDish = ''
+var mealsArray;
+var buttonHasFocus;
 
-var sideButton = document.querySelector('#side-button')
+var sidesButton = document.querySelector('#side-button')
+var mainDishesButton = document.querySelector('#main-dish-button')
+var dessertsButton = document.querySelector('#dessert-button')
+var entireMealButton = document.querySelector('#entire-meal-button')
+var letsCookButton = document.querySelector('#lets-cook-button')
+var clearButton = document.querySelector('#clear-button')
+
 var showRecipe = document.querySelector('#show-recipe-box')
 var recipeSuggestion = document.querySelector('#recipe-suggestion')
 var cookPot = document.querySelector('svg')
-var letsCookButton = document.querySelector('#lets-cook-button')
+
+sidesButton.addEventListener('click', chooseArray)
+mainDishesButton.addEventListener('click', chooseArray)
+dessertsButton.addEventListener('click', chooseArray)
+entireMealButton.addEventListener('click', chooseArray)
+
 letsCookButton.addEventListener('click', showRecipes)
-
-
-function showRecipes() {
-    var randomRecipe = chooseRandomIndex(sides)
-    currentDish = (sides[randomRecipe])
-    showRecipe.innerText = 'You should make: '
-    recipeSuggestion.innerText = `${currentDish}!`
-    cookPot.innerHTML.toggle('hidden')
-}
+// clearButton = document.querySelector('click', resetSuggestions)
 
 function chooseRandomIndex(array) {
     var randomIndex = Math.floor(Math.random() * array.length)
     return randomIndex
 }
+
+function showRecipes() {
+    cookPot.classList.add('hidden')
+    clearButton.classList.remove('hidden')
+
+    if (mealsArray !== 'Choose Entire Meal') {
+    var randomRecipe = chooseRandomIndex(mealsArray)
+        currentDish = mealsArray[randomRecipe]
+    
+    showRecipe.innerText = 'You should make: '
+    recipeSuggestion.innerText = `${currentDish}!`
+    } else {
+        var randomSide = chooseRandomIndex(sides)
+        var sideDish = sides[randomSide]
+        var randomMain = chooseRandomIndex(mainDishes)
+        var mainDish = mainDishes[randomMain]
+        var randomDessert = chooseRandomIndex(desserts)
+        var dessertDish = desserts[randomDessert]
+
+        showRecipe.innerText = 'You should make: '
+        recipeSuggestion.innerText = `${sideDish}, ${mainDish}, and ${dessertDish}!`
+    }
+}
+
+function chooseArray() {
+    buttonHasFocus = document.activeElement
+    if (buttonHasFocus.id === 'side-button') {
+        mealsArray = sides
+    } else if (buttonHasFocus.id === 'main-dish-button') {
+        mealsArray = mainDishes
+    } else if (buttonHasFocus.id === 'dessert-button') {
+        mealsArray = desserts
+    } else { 
+        mealsArray = 'Choose Entire Meal'
+    }
+    return mealsArray
+}
+
